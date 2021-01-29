65a16d6447ce03835334ceec9963bdf9cf65b6ba
Andy Chou <andychou902@gmail.com>
2021-01-29 13:52:11 -0800

    Set up homeVM, API endpoints and load api call skeleton code

--- GithubViewer/ViewModel/HomeViewModel.swift
+++ GithubViewer/ViewModel/HomeViewModel.swift
@@ -8,8 +8,28 @@
 import Foundation
 import SwiftUI
 
+
+let API_ENDPOINT = URL(string: "https://api.github.com/repos/andy168chipz/GithubViewer/commits")!
 class HomeViewModel: ObservableObject {
     
+    @Published var commitData:[CommitData] = [CommitData]()
+    @Published var isLoading = false
     
+    init() {
+        loadCommits(url: API_ENDPOINT)
+    }
     
+    func loadCommits(url: URL){
+        self.isLoading = true
+        URLSession.shared.dataTask(with: url) { (data,resp,error) in
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            do {
+                
+            } catch {
+                
+            }
+            
+        }.resume()
+    }
 }
