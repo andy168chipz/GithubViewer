cfe89c5ffad8d544848a123dc79e37c0e13312d3
Andy Chou <andychou902@gmail.com>
2021-01-28 19:37:15 -0800

    Initial Commit

---
+++ GithubViewer/GithubViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubViewerApp.swift
+//  GithubViewer
+//
+//  Created by Andy Chou on 1/28/21.
+//
+
+import SwiftUI
+
+@main
+struct GithubViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
