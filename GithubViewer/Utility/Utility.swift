f631cbcf7efe9fbeb753a89a25745843a1b060db
Andy Chou <andychou902@gmail.com>
2021-01-29 13:42:10 -0800

    util method implementation

--- GithubViewer/Utility/Utility.swift
+++ GithubViewer/Utility/Utility.swift
@@ -12,6 +12,21 @@ class Utility {
     // Util method to convert date to a human readable string
     static func timeFormatter(date:Date) -> String {
         
-        return ""
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "h:mm a"
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        let elapsedTimeInSeconds = Date().timeIntervalSince(date)
+        if elapsedTimeInSeconds < 0 {
+            return ""
+        }
+        let secondsInDays:TimeInterval = 3600 * 24
+        if elapsedTimeInSeconds > 7 * secondsInDays{
+            dateFormatter.dateFormat = "MM/dd/yy"
+        }else if elapsedTimeInSeconds >  secondsInDays{
+            dateFormatter.dateFormat = "EEEE"
+        }
+        return dateFormatter.string(from:date)
     }
+    
+    
 }
