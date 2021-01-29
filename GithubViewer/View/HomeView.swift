733d29bbab64827e1da08101a5d57950b3d28149
Andy Chou <andychou902@gmail.com>
2021-01-29 15:14:20 -0800

    Wrap contents in scrollview

--- GithubViewer/View/HomeView.swift
+++ GithubViewer/View/HomeView.swift
@@ -11,17 +11,16 @@ struct HomeView: View {
     @ObservedObject var homeVM = HomeViewModel()
     
     var body: some View {
-        VStack{
-            HStack {
-                Text("Author").frame(maxWidth:.infinity).border(Color.black)
-                Text("Commit Hash").frame(maxWidth:.infinity).border(Color.black)
-                Text("Message").frame(maxWidth:.infinity).border(Color.black)
-            }.frame(maxWidth: .infinity)
-            ForEach(self.homeVM.commitData, id:\.sha) {
-                commitData in
-                CommitRow(commitData: commitData).frame(maxWidth:.infinity)
-            }
-        }.frame(maxWidth:.infinity)
+        
+        ScrollView{
+            VStack{
+                ForEach(self.homeVM.commitData, id:\.sha) {
+                    commitData in
+                    CommitRow(commitData: commitData).frame(maxWidth:.infinity)
+                }
+            }.frame(maxWidth:.infinity)
+            
+        }
         
         
     }
