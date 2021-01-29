92ce8338d404457edea2c3c34c4c60e4e14ca61d
Andy Chou <andychou902@gmail.com>
2021-01-29 15:24:28 -0800

    Add paddings to make the UI more readable

--- GithubViewer/View/CommitRow.swift
+++ GithubViewer/View/CommitRow.swift
@@ -13,12 +13,12 @@ struct CommitRow: View {
     
     //Displays those commits in a list with the author, commit hash, and commit message
     var body: some View {
-        VStack {
-            Text(commitData.commit.message).frame(maxWidth:.infinity, alignment: .leading).padding(.horizontal, 8)
+        VStack(spacing: 8) {
+            Text(commitData.commit.message).font(.system(size:18)).frame(maxWidth:.infinity, alignment: .leading).padding(EdgeInsets(top: 8, leading: 8, bottom: 0, trailing: 8))
             HStack {
-                Text(commitData.commit.author.name)
-                Text(commitData.sha).frame(maxWidth:.infinity)
-            }.padding(.horizontal, 8)
+                Text(commitData.commit.author.name).font(.system(size:12))
+                Text(commitData.sha).frame(maxWidth:.infinity).font(.system(size:10))
+            }.padding(EdgeInsets(top: 0, leading: 8, bottom: 8, trailing: 8))
         }.frame(maxWidth:.infinity).border(Color.black)
     }
 }
